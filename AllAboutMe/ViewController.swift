2baa3a58ae670c7d55858ed2e9b7910568086a34
Johnny Hicks <johnnydhicks@gmail.com>
2018-11-01 19:59:39 -0600

    Initial Commit

---
+++ AllAboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AllAboutMe
+//
+//  Created by Johnny Hicks on 11/1/18.
+//  Copyright © 2018 Johnny Hicks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
